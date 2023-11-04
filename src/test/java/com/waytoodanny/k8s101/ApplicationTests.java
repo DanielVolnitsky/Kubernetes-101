1c7429300ce897fffe8ba1318932929c22bd7f41
Daniel Volnitsky <waytodanny@gmail.com>
2023-11-04 17:18:06 +0100

    add project backbone

---
+++ src/test/java/com/waytoodanny/k8s101/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waytoodanny.k8s101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
