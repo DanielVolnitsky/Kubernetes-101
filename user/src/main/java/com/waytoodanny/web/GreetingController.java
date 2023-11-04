7415b6a574c6e146349dc941a4e0f3b4768d35f2
Daniel Volnitsky <waytodanny@gmail.com>
2023-11-04 17:55:22 +0100

    add greeting controller to each service

---
+++ user/src/main/java/com/waytoodanny/web/GreetingController.java
@@ -0,0 +1,13 @@
+package com.waytoodanny.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Hello from the user service!";
+    }
+}
