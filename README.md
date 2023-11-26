3f6989211d7ad3eb0ab527602d06cd5cd02f1909
Daniel Volnitsky <waytodanny@gmail.com>
2023-11-26 16:30:09 +0100

    add environment variable from ConfigMap usage example; add Secret usage example; add PostgreSQL init scripts from ConfigMap example

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # k8s-learning
+
+### Start up greeting service
+```
+kubectl apply -f greeting/k8s/configmap.yml -f greeting/k8s/deployment.yml -f greeting/k8s/service.yml
+```
+
+### Start up greeting service DB
+```
+kubectl apply -f greeting/k8s/db/postgres-secret.yml -f greeting/k8s/db/postgres-configmap-init.yml -f greeting/k8s/db/postgres-statefulset.yml -f greeting/k8s/db/postgres-service.yml
+```
+
+### Additional
+#### Connect to the PostgreSQL DB 
+```
+psql -U <username> -h <hostname> -d <database>
+```
--- greeting/src/main/java/com/waytoodanny/web/GreetingController.java
+++ greeting/src/main/java/com/waytoodanny/web/GreetingController.java
@@ -3,11 +3,14 @@ package com.waytoodanny.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 public class GreetingController {
 
     @GetMapping("/")
     public String greeting() {
-        return "Hello from the greeting service!";
+        var currentEnv = Optional.ofNullable(System.getenv("ENVIRONMENT")).orElse("unknown");
+        return String.format("Hello from the greeting service on %s environment!", currentEnv);
     }
 }
